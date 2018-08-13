daaafce74ef82c4138fb40792d38983215b46c64
爱抠鼻屎的怪大婶 <18231218@qq.com>
2018-08-13 13:54:45 +0800

    动态添加表格数据(修改列颜色未完成)

--- src/view/component/tableList.vue
+++ src/view/component/tableList.vue
@@ -1,20 +1,12 @@
 <template id="tableList">
   <el-table
     :data="tableData"
-    style="width: 100%">
+    style="width: 100%"
+    :header-cell-style="headerColor">
     <el-table-column
-      prop="date"
-      label="日期"
-      align="center">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      align="center">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址"
+      v-for="title in titles"
+      v-bind:prop="title.prop"
+      v-bind:label="title.label"
       align="center">
     </el-table-column>
   </el-table>
@@ -23,25 +15,15 @@
 <script>
     export default {
         name: "tableList",
+        props:['titles','tableData'],
         data() {
         return {
-          tableData: [{
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-          }, {
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄'
-          }, {
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
-          }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }]
+
+        }
+      },
+      methods: {
+        headerColor(row, rowIndex) {
+          return 'background:#e70012;color:#fff';
         }
       }
     }
--- src/view/sales/order_list.vue
+++ src/view/sales/order_list.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="main">
     <breadcrumb menu1="我的订单" menu2="订单列表"></breadcrumb>
-    <tableList></tableList>
+    <tableList :titles="titles" :tableData="tableData"></tableList>
   </div>
 </template>
 
@@ -16,12 +16,106 @@
         },
         data () {
           return{
-
+            titles:[
+              /*表头*/
+              { prop:'order_id',
+                label:"订单编号"
+              },
+              { prop:'name',
+                label:"客户名称"
+              },
+              { prop:'service_type',
+                label:"业务类型"
+              },
+              { prop:'partner',
+                label:"合伙人"
+              },
+              { prop:'line',
+                label:"线索"
+              },
+              { prop:'area',
+                label:"区域"
+              },
+              { prop:'add_time',
+                label:"提交时间"
+              },
+              { prop:'edit_time',
+                label:"最后修改时间"
+              },
+              { prop:'order_status',
+                label:"订单提交状态"
+              },
+              { prop:'pact_status',
+                label:"合同状态"
+              },
+              { prop:'progress',
+                label:"当前进度"
+              },
+              { prop:'tools',
+                label:"操作"
+              }
+            ],
+            /*表格数据*/
+            tableData:[{
+              order_id: '01',
+              name: '王小虎',
+              service_type: '税筹-自有业务-一次性付款',
+              partner:'xx有限公司',
+              line:'/',
+              area:'贵港',
+              add_time:'2018-08-13',
+              edit_time:'2018-08-13',
+              order_status:'已提交',
+              pact_status:'正办理',
+              progress:'未签合同',
+              tools:'操作'
+            }, {
+              order_id: '02',
+              name: '王小虎',
+              service_type: '税筹-自有业务-一次性付款',
+              partner:'xx有限公司',
+              line:'/',
+              area:'贵港',
+              add_time:'2018-08-13',
+              edit_time:'2018-08-13',
+              order_status:'已提交',
+              pact_status:'正办理',
+              progress:'未签合同',
+              tools:'操作'
+            }, {
+              order_id: '03',
+              name: '王小虎',
+              service_type: '税筹-自有业务-一次性付款',
+              partner:'xx有限公司',
+              line:'/',
+              area:'贵港',
+              add_time:'2018-08-13',
+              edit_time:'2018-08-13',
+              order_status:'已提交',
+              pact_status:'正办理',
+              progress:'未签合同',
+              tools:'操作'
+            }, {
+              order_id: '04',
+              name: '王小虎',
+              service_type: '税筹-自有业务-一次性付款',
+              partner:'xx有限公司',
+              line:'/',
+              area:'贵港',
+              add_time:'2018-08-13',
+              edit_time:'2018-08-13',
+              order_status:'已提交',
+              pact_status:'正办理',
+              progress:'未签合同',
+              tools:'操作'
+            }]
           }
         }
     }
 </script>
 
 <style scoped>
-
+  .el-table_4_column_12{
+    color: red;
+  }
 </style>
