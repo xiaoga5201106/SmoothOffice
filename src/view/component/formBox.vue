6bc1ca70d4f0dce066cfeeb27672b0112e205fd4
e <997905119@qq.com>
2018-08-22 16:36:21 +0800

    模态框按钮&&表单组件&&表单按钮组件

---
+++ src/view/component/formBox.vue
@@ -0,0 +1,41 @@
+<template id="formBox">
+  <div class="formBox">
+      <el-form   label-width="150px" label-position="left">
+    <el-form-item 
+    v-for="label in labels"
+     :label="label.title">
+    <el-input v-if="label.key=='1'" style="width:400px" :placeholder="'请输入'+label.title"></el-input>
+    <el-select v-if="label.key=='2'" style="width:400px" v-model="label.value">
+      <el-option 
+         v-for="item in label.items"
+        :label="item.item"
+         v-model="item.item">  
+      </el-option>
+    </el-select>
+   </el-form-item>
+</el-form>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name:'formBox',
+    props:["labels"],
+    data() {
+      return {
+           
+      }
+    },
+    methods: {
+      onSubmit() {
+        console.log('1111');
+      }
+    }
+  }
+</script>
+<style scoped>
+  .el-form-item{
+     padding: 10px 0 10px;
+  }
+</style>
---
+++ src/view/component/formButton.vue
@@ -0,0 +1,20 @@
+<template id="formButton">
+	<div class="formButton">
+ <el-button v-if="state1=='通过'" type="primary">{{state1}}</el-button>
+   <el-button v-if="state2=='不通过'" type="danger" >{{state2}}</el-button>
+   <el-button v-if="state3=='上传'" type="danger" >{{state3}}</el-button>
+   <el-button v-if="state4=='提交'" type="danger">{{state4}}</el-button>
+	</div>
+</template>
+
+<script >
+	export default{
+		name:'formButton',
+		props:["state1","state2","state3","state4"],
+		data(){
+			return{
+
+			}
+		}
+	}
+</script>
